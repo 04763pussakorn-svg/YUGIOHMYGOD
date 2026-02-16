c68e168ef1bc134a4006699dbeede95d1fa51f9f
Kittipat <kittiphat_namjak@cmu.ac.th>
2026-02-16 23:04:30 +0700

    remove Class_Card function and define Card C array variable

--- class.h
+++ class.h
@@ -22,6 +22,7 @@ public:
         if(type == "Monster") cout << "\033[38;5;94m";
         else if(type == "Spell") cout << "\033[0;32m";
         else if(type == "Trap") cout << "\033[0;35m";
+        else ;
 
         cout << "\n====================\n";
         cout << "Name  : " << name << endl;
@@ -36,20 +37,19 @@ public:
     }
 };
 
-Card Class_Card(int a){
+
 
     Card C[10];
 
-    C[0] = Card("Beaver Warrior", "Monster", 4, 1200, 1500, "A beaver that is skilled in combat.");
-    C[1] = Card("Celtic Guardian", "Monster", 4, 1400, 1200, "A warrior with a Celtic sword and shield.");
-    C[2] = Card("Curse of Dragon", "Monster", 5, 2000, 1500, "A dragon that is said to be cursed.");
-    C[3] = Card("Dark Magician", "Monster", 7, 2500, 2100, "The ultimate wizard.");
-    C[4] = Card("Feral Imp", "Monster", 4, 1300, 1400, "A mischievous imp.");
-    C[5] = Card("Gaia The Fierce Knight", "Monster", 7, 2300, 2100, "A fierce knight.");
-    C[6] = Card("Giant Soldier of Stone", "Monster", 3, 1300, 2000, "A stone giant.");
-    C[7] = Card("Griffore", "Monster", 4, 1600, 1000, "A swift griffin.");
-    C[8] = Card("Pot of Greed", "Spell", 0, 0, 0, "Draw 2 cards.");
-    C[9] = Card("Mirror Force", "Trap", 0, 0, 0, "Destroy all attack position monsters your opponent controls.");
+   Card C[0] = Card("Beaver Warrior", "Monster", 4, 1200, 1500, "A beaver that is skilled in combat.");
+   Card C[1] = Card("Celtic Guardian", "Monster", 4, 1400, 1200, "A warrior with a Celtic sword and shield.");
+   Card C[2] = Card("Curse of Dragon", "Monster", 5, 2000, 1500, "A dragon that is said to be cursed.");
+   Card C[3] = Card("Dark Magician", "Monster", 7, 2500, 2100, "The ultimate wizard.");
+   Card C[4] = Card("Feral Imp", "Monster", 4, 1300, 1400, "A mischievous imp.");
+   Card C[5] = Card("Gaia The Fierce Knight", "Monster", 7, 2300, 2100, "A fierce knight.");
+   Card C[6] = Card("Giant Soldier of Stone", "Monster", 3, 1300, 2000, "A stone giant.");
+   Card C[7] = Card("Griffore", "Monster", 4, 1600, 1000, "A swift griffin.");
+   Card C[8] = Card("Pot of Greed", "Spell", 0, 0, 0, "Draw 2 cards.");
+   Card C[9] = Card("Mirror Force", "Trap", 0, 0, 0, "Destroy all attack position monsters your opponent controls.");
    
-    return C[a];
-}
+
