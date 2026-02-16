d6b8a6eaf4efa4e16ed89df65b446826f21131bf
Kittipat <kittiphat_namjak@cmu.ac.th>
2026-02-16 22:04:24 +0700

    test Class_Card

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
+#include <iostream>  
+#include <Windows.h>  
+#include <sstream>
+#include <iomanip> 
+
 #include "display.h"
+#include "class.h"
 
 int main() {
   
-        updateScreen();
-        Sleep(500);
-
+    updateScreen();
+    Sleep(500);
     
-    cout << "Game End";
+    cout << Class_Card(1).name;
     
     return 0;  
 }  
